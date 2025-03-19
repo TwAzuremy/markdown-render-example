028189460535a9c0333e8ce5e3445a7e7b890051
TwAzuremy <958559114@qq.com>
2025-03-20 00:16:24 +0800

    feat (markdown): Add a range of features.
    - When the cursor is over an element, the markdown sign can be displayed. ( The current testing phase, replaced by a color change )

--- src/components/MarkdownEditor.jsx
+++ src/components/MarkdownEditor.jsx
@@ -4,12 +4,16 @@ import ContentEditable from "react-contenteditable";
 import { memo, useEffect, useRef, useState } from "react";
 
 import { parseMarkdownString, parseHTMLElement } from "../renderer/MarkdownRenderer";
-import { saveCursorSelection, restoreCursorSelection } from "../utils/CursorUtils";
+import { saveCursorSelection, restoreCursorSelection, getElementUnderCursor } from "../utils/CursorUtils";
+import { findClosestElement, findFurthestElement } from "../utils/DOMUtils";
 import { debounce } from "../utils/BaseUtils";
 
 const MarkdownEditor = memo(({ md = "", ...props }) => {
     /** @type {React.RefObject<Node|null>} */
     const editorElementRef = useRef(null);
+    /** @type {React.RefObject<Node|null>} */
+    const lastHasSignDisplayElementRef = useRef(null);
+
     /** @type {[content: String|null, setContent: React.Dispatch<React.SetStateAction<String|null>>]} */
     const [content, setContent] = useState(null);
     const [cursor, setCursor] = useState({ start: 0, end: 0 });
@@ -29,6 +33,55 @@ const MarkdownEditor = memo(({ md = "", ...props }) => {
         setCursor(selection);
     }, 100);
 
+    /**
+     * Handle the `blur` event and clear the markdown signs display status.
+     * This function is called when an element loses focus.
+     * 
+     * @function
+     * @returns {void}
+     */
+    const handleBlur = () => {
+        // Clear the display state of markdown signs by passing `null` to `renderSigns`.
+        renderSigns(null);
+    };
+
+    /**
+     * Handles the `keyup` event and prevents the default action.
+     * This is used to prevent the `Keyup` from affecting the active state of an HTML element.
+     * 
+     * @function
+     * @param {Event} event - The keyboard event object.
+     * @returns {void}
+     */
+    const handleKeyUp = (event) => {
+        // Cancel the keyboard key release event, as this may affect the active state of the HTML element.
+        event.preventDefault();
+    };
+
+    /**
+     * Handles the cursor movement and updates the rendering of markdown signs.
+     * It identifies the element under the cursor and determines if it's a markdown inline or block element.
+     * Based on that, it calls the appropriate rendering logic.
+     * 
+     * @function
+     * @returns {void}
+     */
+    const handleCursorMove = () => {
+        // Get the element under the cursor.
+        const elementUnderCursor = getElementUnderCursor(editorElementRef.current);
+
+        // Start by looking for the farthest `markdown-inline` element.
+        let container = findFurthestElement(elementUnderCursor, ".md-i", true);
+
+        if (!container) {
+            // If no farthest `markdown-inline` element is found, look for the nearest `markdown-block` element.
+            container = findClosestElement(elementUnderCursor, ".md-b", true);
+        }
+
+        // Controls whether the markdown sign is rendered based on the container found.
+        renderSigns(container);
+    };
+
     useEffect(() => {
         (async () => {
             setContent(await parseMarkdownString(md));
@@ -36,8 +89,45 @@ const MarkdownEditor = memo(({ md = "", ...props }) => {
     }, [md]);
 
     useEffect(() => {
-        restoreCursorSelection(editorElementRef.current, cursor);
+        // It is possible that the `cursor` is null, so the default value is set.
+        restoreCursorSelection(editorElementRef.current, cursor || { start: 0, end: 0 });
+        // When the cursor moves, you can also control the display of the markdown sign.
+        requestAnimationFrame(handleCursorMove);
     }, [cursor]);
+
+    /**
+     * Renders the sign display by toggling the "sign-display" class between the current container 
+     * and the last element that had the "sign-display" class. The function now includes a `compulsion` 
+     * parameter that forces the class to be applied even if the container is the same as the last one.
+     * 
+     * @param {Element} container - The DOM element that should receive the "sign-display" class.
+     * @param {Boolean} [compulsion=false] - A flag that, when set to true, forces the sign-display update even 
+     *      when the container is the same as the last one.
+     * 
+     * @example
+     * // Rendering is not forced.
+     * renderSigns(markdownElement);
+     * 
+     * // Forced rendering.
+     * renderSigns(markdownElement, true);
+     */
+    function renderSigns(container, compulsion = false) {
+        // Get the reference to the last element that had the "sign-display" class
+        const lastElement = lastHasSignDisplayElementRef.current;
+
+        // If `compulsion` is false and the container is the same as the last element or their textContent 
+        // matches, do nothing
+        if (!compulsion && (container === lastElement ||
+            lastElement?.textContent === container?.textContent)) return;
+
+        // Remove the "sign-display" class from the last element if it exists
+        lastElement?.classList.remove("sign-display");
+        // Add the "sign-display" class to the current container element
+        container?.classList.add("sign-display");
+
+        // Update the reference to the current container as the last element with the "sign-display" class
+        lastHasSignDisplayElementRef.current = container || null;
+    }
 
     return (
         <ContentEditable
@@ -46,7 +136,11 @@ const MarkdownEditor = memo(({ md = "", ...props }) => {
             innerRef={editorElementRef}
             html={content}
             tagName={"div"}
-            onChange={handleChange} />
+            onChange={handleChange}
+            onBlur={handleBlur}
+            onKeyUp={handleKeyUp}
+            onKeyDown={() => requestAnimationFrame(handleCursorMove)}
+            onMouseUp={() => requestAnimationFrame(handleCursorMove)} />
     );
 });
 
